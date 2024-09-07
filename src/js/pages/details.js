5fe4e289a85a4e2855760373ddb6de3a103bda0c
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-09-08 02:59:29 +0800

    Add trailer

--- src/js/pages/details.js
+++ src/js/pages/details.js
@@ -1,7 +1,7 @@
 //Importing API Key and Image Endpoint from the config module
 
 import { apiKey } from '../modules/config.js';
-import { imgEndpoint } from '../modules/config.js'
+import { imgEndpoint } from '../modules/config.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function fetchShowDetails(showId) {
-    const detailsUrl = `https://api.themoviedb.org/3/tv/${showId}?api_key=${apiKey}&language=en-US&append_to_response=credits`;
+    const detailsUrl = `https://api.themoviedb.org/3/tv/${showId}?api_key=${apiKey}&language=en-US&append_to_response=credits,videos`;
 
     fetch(detailsUrl)
         .then(response => {
@@ -25,28 +25,36 @@ function fetchShowDetails(showId) {
             return response.json();
         })
         .then(data => {
+    
+            const trailer = data.videos.results.find(video => video.type === 'Trailer');
+            const trailerEmbedUrl = trailer ? `https://www.youtube.com/embed/${trailer.key}` : '';
+
             const detailsHTML = `
                 <h2 class="show-title">${data.name}</h2>
+                 ${trailerEmbedUrl ? `
+                <div class="trailer-container">
+                    <iframe width="560" height="315" src="${trailerEmbedUrl}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>` : ''}
                 <p class="details-container"><strong>Description:</strong> ${data.overview}</p>
                 <p class="details-container"><strong>Genres:</strong> ${data.genres.map(genre => genre.name).join(', ')}</p>
                 <p class="details-container"><strong>Airing Date:</strong> ${data.first_air_date}</p>
                 <p class="details-container"><strong>Seasons:</strong> ${data.number_of_seasons}</p>
                 <p class="details-container"><strong>Episodes:</strong> ${data.number_of_episodes}</p>
                 <p class="details-container"><strong>Actors:</strong></p>
-        <div class="actor-list">
-            ${data.credits.cast.slice(0, 5).map(actor => `
-                <div class="actor-card">
-                    <img src="${actor.profile_path ? imgEndpoint + actor.profile_path : 'https://via.placeholder.com/150'}" alt="${actor.name}">
-                    <p>${actor.name}</p>
+                <div class="actor-list">
+                    ${data.credits.cast.slice(0, 5).map(actor => `
+                        <div class="actor-card">
+                            <img src="${actor.profile_path ? imgEndpoint + actor.profile_path : 'https://via.placeholder.com/150'}" alt="${actor.name}">
+                            <p>${actor.name}</p>
+                        </div>
+                    `).join('')}
                 </div>
-            `).join('')}
-        </div>
             `;
+
             document.querySelector('.tv-show-details').innerHTML = detailsHTML;
         })
         .catch(error => console.error('Error fetching show details:', error));
 }
-
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.main-content').classList.add('fade-in');
