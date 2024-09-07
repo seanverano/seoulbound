1a35eb6df5c7388edab594e457a00871b4e8a3a5
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-09-08 04:30:06 +0800

    Add the click to redirect function in genres page

--- src/js/pages/genres.js
+++ src/js/pages/genres.js
@@ -1,7 +1,10 @@
 // Importing API Key and Image Endpoint from the config module
+
 import { apiKey } from '../modules/config.js';
 import { imgEndpoint } from '../modules/config.js';
 import { generateCardHTMLForAdd } from '../modules/card.js';
+
+// Function to fetch K-Dramas by genre
 
 function fetchKDramaByGenre(genreId) {
     const genreUrl = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&with_genres=${genreId}&with_original_language=ko&language=en-US`;
@@ -10,10 +13,34 @@ function fetchKDramaByGenre(genreId) {
         .then(response => response.json())
         .then(data => {
             const kdramaCardsHTML = data.results.map(show => generateCardHTMLForAdd(show, imgEndpoint)).join('');
-            document.querySelector('.kdrama-cards-container').innerHTML = kdramaCardsHTML;
+            const container = document.querySelector('.kdrama-cards-container');
+            if (container) {
+                container.innerHTML = kdramaCardsHTML;
+                addImageClickListeners();
+            }
         })
         .catch(error => console.error('Error fetching K-Dramas:', error));
 }
+
+// When user click an image/poster, it will redirect you to the details page
+
+function addImageClickListeners() {
+    document.querySelectorAll('.card img').forEach(img => {
+        img.addEventListener('click', function(event) {
+            const target = event.target;
+            const card = target.closest('.card');
+            
+            if (card) {
+                const showId = card.getAttribute('data-id');
+                if (showId) {
+                    window.location.href = `details.html?id=${showId}`;
+                }
+            }
+        });
+    });
+}
+
+//Handles the k-drama selection through genre filtering
 
 document.getElementById('genreSelect').addEventListener('change', function() {
     const selectedGenre = this.value;
@@ -27,7 +54,6 @@ document.getElementById('genreSelect').addEventListener('change', function() {
 document.addEventListener('DOMContentLoaded', function() {
     document.body.addEventListener('click', function(event) {
         if (event.target.classList.contains('card-button')) {
-            
             const card = event.target.closest('.card');
             if (card) {
                 const item = {
@@ -50,8 +76,9 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
+//for fading transition
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.main-content').classList.add('fade-in');
 });
+
