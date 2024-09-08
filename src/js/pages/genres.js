1577997daa58e8d38f0491f2973f6d769ef8df5c
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-09-08 23:32:04 +0800

    Remove pagination in genres and adjust maximum displayed cards

--- src/js/pages/genres.js
+++ src/js/pages/genres.js
@@ -5,15 +5,24 @@ import { imgEndpoint } from '../modules/config.js';
 import { generateCardHTMLForAdd } from '../modules/card.js';
 
 // Function to fetch K-Dramas by genre
+// Also filters out the reality, variety, and other non-kdrama 
+// Sorts out by popularity so the current popular k-drama will populate first
+// Limits the number of cards being displayed by using slice and assigning maximum cards
 
-function fetchKDramaByGenre(genreId) {
-    const genreUrl = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&with_genres=${genreId}&with_original_language=ko&language=en-US`;
+function fetchKDramaByGenre(genreId, maxCards = 16) {
+    const genreUrl = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&with_genres=${genreId}&with_original_language=ko&language=en-US&sort_by=popularity.desc`;
 
     fetch(genreUrl)
         .then(response => response.json())
         .then(data => {
-            const kdramaCardsHTML = data.results.map(show => generateCardHTMLForAdd(show, imgEndpoint)).join('');
+            const kdramas = data.results.filter(show => {
+                return show.genre_ids.includes(18) && !show.genre_ids.includes(10764) && !show.genre_ids.includes(10767);
+            });
+            const sortedKdramas = kdramas.sort((a, b) => b.popularity - a.popularity);
+            const limitedKdramas = sortedKdramas.slice(0, maxCards);
+            const kdramaCardsHTML = limitedKdramas.map(show => generateCardHTMLForAdd(show, imgEndpoint)).join('');
             const container = document.querySelector('.kdrama-cards-container');
+
             if (container) {
                 container.innerHTML = kdramaCardsHTML;
                 addImageClickListeners();
@@ -21,6 +30,7 @@ function fetchKDramaByGenre(genreId) {
         })
         .catch(error => console.error('Error fetching K-Dramas:', error));
 }
+
 
 // When user click an image/poster, it will redirect you to the details page
 
