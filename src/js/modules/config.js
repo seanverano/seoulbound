4403f6ebafa79bea952f37dead121eeb9378db5c
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-09-30 16:25:22 +0800

    Regenerate new API key

--- src/js/modules/config.js
+++ src/js/modules/config.js
@@ -1,4 +1,4 @@
 //This module is used to export API Key and Image Endpoint to other JS files
 
-export const apiKey = 'b3a643714b4535e159b5129c5a6e8180';
-export const imgEndpoint = 'https://image.tmdb.org/t/p/w500';
+export const apiKey = "2a3c21f7203959050cb73bdefd2b2ae2";
+export const imgEndpoint = "https://image.tmdb.org/t/p/w500";
