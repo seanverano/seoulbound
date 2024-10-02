977626272c5195a7977663ac13658d824cdd45bf
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-10-02 21:27:32 +0800

    Added a msg for API

--- src/js/modules/config.js
+++ src/js/modules/config.js
@@ -1,4 +1,7 @@
 //This module is used to export API Key and Image Endpoint to other JS files
 
+// If the website doesnt display the data (ex: kdrama img and details)
+// It means that I already deleted the API key below
+// Just send me a message through my socials so I can regenrate a new key for you
 export const apiKey = "2a3c21f7203959050cb73bdefd2b2ae2";
 export const imgEndpoint = "https://image.tmdb.org/t/p/w500";
