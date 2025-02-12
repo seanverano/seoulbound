057b6086aebe2883253b74ca243ed5bf85d0e5c4
Sean Dustin Verano <verano.seandustin@gmail.com>
2025-02-13 01:56:15 +0800

    Add message in config.js

--- src/js/modules/config.js
+++ src/js/modules/config.js
@@ -1,4 +1,8 @@
 //This module is used to export API Key and Image Endpoint to other JS files
 
-export const apiKey = 'b3a643714b4535e159b5129c5a6e8180';
-export const imgEndpoint = 'https://image.tmdb.org/t/p/w500';
+// If the website doesnt display the data (ex: kdrama img and details)
+// It means that I already deleted the API key below (both here and in the TMDB database so even if it's in the commit history it's no longer valid)
+// Just send me a message through my socials so I can regenerate a new key for you
+
+export const apiKey = "";
+export const imgEndpoint = "https://image.tmdb.org/t/p/w500";
